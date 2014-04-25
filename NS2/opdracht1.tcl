4f04cea4d4ef88e248afef584d5d0788154dd3ec
WoutV <wout.vekemans@student.kuleuven.be>
2014-04-25 12:25:49 +0200

    update CN

--- NS2/opdracht1.tcl
+++ NS2/opdracht1.tcl
@@ -6,11 +6,11 @@ $ns color 1 Red
 $ns color 2 Green
 
 #trace file
-set tf [open out1.tr w]
+set tf [open opdracht1.tr w]
 $ns trace-all $tf
 
 #nam tracefile
-set nf [open out1.nam w]
+set nf [open opdracht1.nam w]
 $ns namtrace-all $nf
 
 proc finish {} {
@@ -39,7 +39,7 @@ $ns duplex-link $n0 $n2 10Mb 0.2ms DropTail
 $ns duplex-link $n1 $n2 10Mb 0.2ms DropTail
 $ns duplex-link $n2 $n3 10Mb 0.2ms DropTail
 $ns simplex-link $n3 $n4 256kb 0.2ms DropTail
-$ns duplex-link $n4 $n3 4Mb 0.2ms DropTail
+$ns simplex-link $n4 $n3 4Mb 0.2ms DropTail
 $ns duplex-link $n4 $n5 100Mb 0.3ms DropTail
 $ns duplex-link $n5 $n6 100Mb 0.3ms DropTail
 $ns duplex-link $n5 $n7 100Mb 0.3ms DropTail
